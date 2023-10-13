3d0b70c04c7bbaf0aac6463a52a0619d1e8f1af4
AlexandreSantanaa <126908528+AlexandreSantanaa@users.noreply.github.com>
2023-10-13 03:05:36 -0300

    exercicio

--- js/exercicio05.js
+++ js/exercicio05.js
@@ -1,4 +1,10 @@
 function executarExercicio05(){
     alert("5. Escreva um programa que leia o preço de um produto e a quantidade comprada, e calcule o valor total a ser pago com desconto de 10% se a quantidade for maior que 10 unidades.")
-    
+
+    let preco = parseFloat(prompt("Digite o valor do produto"))
+    let quantidade = parseFloat(prompt("Digite a quantidade do produto"))
+
+    let valorTotal = (quantidade>10)?preco*quantidade-(preco*quantidade*0.1):preco*quantidade
+
+    alert("Valor total é R$"+valorTotal)
 }
