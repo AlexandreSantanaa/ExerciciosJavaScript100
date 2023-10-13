799304cef751207e51799e7f5e224f0c2db5844c
AlexandreSantanaa <126908528+AlexandreSantanaa@users.noreply.github.com>
2023-10-13 03:10:39 -0300

    exercicio

--- js/exercicio06.js
+++ js/exercicio06.js
@@ -1,3 +1,9 @@
 function executarExercicio06(){
-    alert("")
+    alert("6. Crie um programa que leia um número inteiro e exiba se ele é par ou ímpar. ")
+
+    let valor = parseInt(prompt("Digite um numero:"))
+
+    let resposta = (valor%2==0)?"par":"impar"
+
+    alert(valor+" é "+resposta)
 }
