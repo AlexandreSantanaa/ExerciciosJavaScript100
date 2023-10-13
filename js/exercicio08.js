4b59423f1c9ae1314b33669fbf9fc9f7f00584a8
AlexandreSantanaa <126908528+AlexandreSantanaa@users.noreply.github.com>
2023-10-13 03:10:53 -0300

    exercicio

--- js/exercicio08.js
+++ js/exercicio08.js
@@ -1,3 +1,5 @@
 function executarExercicio08(){
-    alert("")
+    alert("8. Escreva um programa que leia dois números inteiros e troque os seus valores, ou seja, o primeiro deve ficar com o valor do segundo e vice-versa.")
+
+    
 }
