075bc14fb65f9e7d580d9dab5c9613e2f3044335
AlexandreSantanaa <126908528+AlexandreSantanaa@users.noreply.github.com>
2023-10-13 03:10:49 -0300

    exercicio

--- js/exercicio07.js
+++ js/exercicio07.js
@@ -1,3 +1,8 @@
 function executarExercicio07(){
-    alert("")
+    alert("7. Faça um programa que receba o salário base de um funcionário e calcule o seu salário líquido, considerando que o funcionário recebe 5% de comissão sobre o salário base. ")
+
+    let salarioBase = parseFloat(prompt("Digite seu salario base:"))
+    let salarioLiquido = salarioBase+salarioBase*0.05
+
+    alert("salario liquido:"+salarioLiquido)
 }
